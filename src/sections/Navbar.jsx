5581f76996ef9fd5355fa6cf2e60549ee608ec28
Vorleak Yek <vorleakyek@gmail.com>
2024-12-10 21:55:12 -0800

    finish Navbar component

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
 import { useState } from 'react';
 
+import {navLinks} from '../constants/index';
+
+const NavItems = () => {
+  return (
+    <ul className='nav-ul'>
+      {navLinks.map(({id, href, name})=>(
+        <li key={id} className='nav-li'>
+          <a href={href} className='nav-li_a' onClick={()=>{}}>{name}</a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(prevIsOpen => !prevIsOpen);
 
@@ -13,11 +26,22 @@ const Navbar = () => {
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
             Vorleak
           </a>
+
           <button className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
             aria-label="Toggle menu" onClick={toggleMenu}>
             <img src={isOpen ? "assets/close.svg" :"assets/menu.svg"} alt="toggle" className='w-6 h-6' />
           </button>
+
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
         </div>
+      </div>
+
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems/>
+        </nav>
       </div>
     </header>
   )
