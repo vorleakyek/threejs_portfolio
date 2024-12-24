1f42f2e9e7c076a283e5cac1a4530a646a2f6be8
Vorleak Yek <vorleakyek@gmail.com>
2024-12-23 21:27:08 -0800

    add experience section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Hero from './sections/Hero';
 import About from './sections/About';
 import Contact from './sections/Contact';
 import Footer from './sections/Footer';
+import Experience from './sections/Experience';
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <Hero />
+      <Experience/>
       <About />
       <Contact />
       <Footer/>
---
+++ src/sections/Experience.jsx
@@ -0,0 +1,7 @@
+const Experience = () => {
+  return(
+    <div>hello</div>
+  )
+}
+
+export default Experience;
