2a67f3ccf0452d5a9bf71d72461de674220370fd
Vorleak Yek <vorleakyek@gmail.com>
2024-12-23 21:15:52 -0800

    add Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Contact from './sections/Contact';
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Contact />
+      <Footer/>
     </main>
   )
 }
